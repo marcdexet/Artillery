fca0795d046dba9fc7729e852a9b3d27827409bd
Marc DeXET <marc.dexet@ias.u-psud.fr>
2021-01-01 17:29:34 +0100

    with canon centered

--- artillery/pieces.py
+++ artillery/pieces.py
@@ -1,25 +1,25 @@
+import numpy as np
 import pygame as pg
 from pygame.sprite import Sprite
+
+from artillery.compute import move
 
 
 class Canon(Sprite):
 
     def __init__(self, pos=(0, 0), size=(200, 200)):
         super(Canon, self).__init__()
-        self.original_image = pg.image.load('sprites/canon.png')
+        self.original_image = pg.image.load('sprites/canon_centered.png')
         rec = self.original_image.get_rect()
-        self.pivot = pg.Vector2(rec.width * 2 / 5, rec.height / 2)
-        self.original_vector = pg.Vector2(rec.width * (2 / 5 - 1 / 2), 0.)
-
+        self.original_vector = pg.Vector2(1.,0.)
+        self.vector = self.original_vector
         pg.draw.line(surface=self.original_image, color=(255, 0, 255), start_pos=rec.midleft, end_pos=rec.midright, width=1)
         pg.draw.line(surface=self.original_image, color=(255, 0, 255), start_pos=rec.midtop, end_pos=rec.midbottom, width=1)
-        pg.draw.line(surface=self.original_image, color=(125, 125, 255), start_pos=(self.pivot[0], 0), end_pos=(self.pivot[0], rec.bottom), width=1)
 
         self.image = self.original_image
-        self.vector = self.original_vector
         self.rect = self.image.get_rect()
         self.rect.center = pos
-        self.pos = pg.Vector2(pos)
+        self.pos = pos
         self.angle = 0
         self.required_angle = 0
 
@@ -30,8 +30,34 @@ class Canon(Sprite):
         if self.angle != self.required_angle:
             orientation = 1 if (self.angle - self.required_angle < 0 ) else -1
             self.angle += orientation
-            rotated = self.original_vector.rotate(self.angle)
-            offset = rotated - self.original_vector
             self.image = pg.transform.rotate(self.original_image, self.angle)
-            print(f'ROTATE=({rotated}, len={rotated.length()}), OFFSET=({offset}, len={offset.length()}), ORIG={self.original_image.get_rect().center}, ROTATED={self.image.get_rect().center}')
-            self.rect = self.image.get_rect(center=self.pos + offset  - self.original_vector)
+            self.vector = self.original_vector.rotate(self.angle)
+            self.rect = self.image.get_rect(center=self.pos)
+
+class Target(Sprite):
+
+    def __init__(self, pos=(0, 0), size=(20,5)):
+        super(Target, self).__init__()
+        self.image = pg.Surface(size=size)
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+        self.pos = pos
+
+
+class Ball(Sprite):
+
+    def __init__(self,pos=(0, 0), size=(5,5)):
+        super(Ball, self).__init__()
+        self.image = pg.Surface(size=size)
+        self.rect = pg.draw.circle(self.image, pg.Color("red"), pos, 5)
+        self.rect.center = pos
+        self.pos = pos
+        self._array = np.zeros((2,2),dtype=float)
+
+    def set_initial_speed(self, speed):
+        self._array[1,:] = speed
+
+    def update(self, *args, **kwargs) -> None:
+        ctx = kwargs['ctx']
+        move(self._array, ctx)
--- artillery_game.py
+++ artillery_game.py
@@ -1,9 +1,9 @@
 from typing import Tuple
 
 import pygame as pg
-
+import numpy as np
 from artillery.compute import Context, compute, new_ball, move
-from artillery.pieces import Canon
+from artillery.pieces import Canon, Target
 
 RED = (150, 150, 150)
 
@@ -11,8 +11,9 @@ RED = (150, 150, 150)
 def init_game() -> Tuple[pg.Surface, pg.time.Clock]:
     pg.init()
     clock = pg.time.Clock()
-    screen = pg.display.set_mode((800, 800))
-    return screen, clock
+    screen = pg.display.set_mode((1600, 600))
+    font = pg.font.Font(None, 25)
+    return screen, clock, font
 
 
 def draw_landscape(screen):
@@ -32,16 +33,21 @@ def draw_ball(screen, ball):
 
 def run_game():
     done = False
-    screen, clock = init_game()
+    screen, clock, font = init_game()
     ball = None
     ctx = Context()
-
+    delta_angle = 1.
+    FONT_COLOR = (255,255,255)
     def stop_fn(p):
         p[0][1] < 0
 
-    canon = Canon(pos=(40,740),size=(50, 50))
+    canon = Canon(pos=(40,screen.get_height() - 20),size=(50, 50))
+    target = Target(pos=(1000, screen.get_height() - 20))
     all_sprites = pg.sprite.Group(canon)
+    all_sprites.add(target)
     required_angle = 0
+    spring = 10.
+    coef_spring = 1.
 
     while not done:
 
@@ -56,7 +62,13 @@ def run_game():
 
         all_sprites.update()
         all_sprites.draw(screen)
-#        screen.blit(canon.image, canon.rect)
+
+        text_angle = font.render(str("Angle: %.2f " % canon.required_angle), True, FONT_COLOR)
+        screen.blit(text_angle, (10, 10))
+        text_spring = font.render(str("Force: %.2f " % spring), True, FONT_COLOR)
+        screen.blit(text_spring, (10, 40))
+        text_coef = font.render(str("Coef.: %.2f " % coef_spring), True, FONT_COLOR)
+        screen.blit(text_coef, (10, 70))
         pg.display.flip()
 
         for e in pg.event.get():
@@ -64,18 +76,35 @@ def run_game():
                 done = 1
                 break
             elif e.type == pg.KEYDOWN:
+                modifying_angle = 0
+                modif_spring = 0
                 if e.key == pg.K_n:
                     ball = new_ball(ctx=ctx)
-                    ball[0, :] = ( canon.x, canon.y)
-                    ball[1, :] = [5., -5.]
+                    ball[0, :] = canon.rect.center
+                    x, y = canon.vector
+                    ball[1, :] = np.array([x, -y]) * spring
+                    print(f'VEC={canon.vector}, l={canon.vector.length()}, BALL={ball}')
                 elif e.key == pg.K_a:
-                    required_angle += 10
-                    print(f'Required {required_angle}')
-                    canon.set_required_elevation_to(required_angle)
+                    modifying_angle += delta_angle
                 elif e.key == pg.K_z:
-                    required_angle -= 10
-                    print(f'Required {required_angle}')
+                    modifying_angle -= delta_angle
+                elif e.key == pg.K_q:
+                    modifying_angle += delta_angle * 5
+                elif e.key == pg.K_s:
+                    modifying_angle -= delta_angle * 5
+                elif e.key == pg.K_w:
+                    modif_spring += 1 * coef_spring
+                elif e.key == pg.K_x:
+                    modif_spring -= 1 * coef_spring
+                elif e.key == pg.K_c:
+                    coef_spring *= 10
+                elif e.key == pg.K_v:
+                    coef_spring /= 10
+                if modifying_angle:
+                    required_angle += modifying_angle
                     canon.set_required_elevation_to(required_angle)
+                if modif_spring:
+                    spring += modif_spring
         clock.tick(40)
 
 
